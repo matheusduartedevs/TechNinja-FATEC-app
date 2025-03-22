cac519a053d536cb12998a27cbba766e1b0a2029
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-03-22 15:46:23 -0300

    chore: remove unnecessary files and refactor structure

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+
+export default function App() {
+    const test = () => {
+        console.log('test');
+    }
+
+    return (
+        <View>
+            <Text>TechNinja</Text>
+        </View>
+    )
+}
