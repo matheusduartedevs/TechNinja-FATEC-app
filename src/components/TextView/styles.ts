1c90dac83c6c57b7a89c7f3dafc555a4475c7a9a
Luan Beserra <luan.oliveira30@fatec.sp.gov.br>
2025-04-02 22:58:36 -0300

    feat: implementing launcher screen

--- src/components/TextView/styles.ts
+++ src/components/TextView/styles.ts
@@ -1,5 +1,10 @@
 import designSystem from "@/src/styles/theme";
+import { TitleViewProps } from "@/src/components/TitleView/props";
 
-export const styles = () => ({
+export const styles = ({ color }: Pick<TitleViewProps, "color">) => ({
   fontSize: designSystem.fonts.textSize,
+  color:
+    color === "primary"
+      ? designSystem.colors.text.primary
+      : designSystem.colors.text.secondary,
 });
