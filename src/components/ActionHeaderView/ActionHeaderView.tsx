c9b8495af635ab7c864bc94a7a98407e6cfd9c09
Luan Beserra <luanbeserraoliveira@gmail.com>
2025-04-06 16:44:07 -0300

    fix: updating ActionHeaderView component props

--- src/components/ActionHeaderView/ActionHeaderView.tsx
+++ src/components/ActionHeaderView/ActionHeaderView.tsx
@@ -4,11 +4,14 @@ import { styles } from "@/src/components/ActionHeaderView/styles";
 import arrowBack from "@/assets/icons/arrow-back.png";
 import TitleView from "@/src/components/TitleView/TitleView";
 
-export default function ActionHeaderView({ style }: ActionHeaderViewProps) {
+export default function ActionHeaderView({
+  style,
+  title,
+}: ActionHeaderViewProps) {
   return (
     <View style={[styles.container, style]}>
       <Image source={arrowBack} style={styles.icon} />
-      <TitleView title={"Ranking"} color={"primary"} style={styles.title} />
+      <TitleView title={title} color={"primary"} style={styles.title} />
     </View>
   );
 }
