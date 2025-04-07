08734115699c16c0f4a402175a55b0aa0e0dee66
PedroCarvalho-Fatec <pedrohenriqueholandaa@hotmail.com>
2025-04-06 21:00:57 -0300

    feat: implementing home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import TitleView from "@/src/components/TitleView/TitleView";
 
+
 export default function App() {
   return (
+    
     <View>
-      <TitleView title={"TechNinja"} color={"secondary"} />
+      <TitleView title="{TechNinja}" color={"secondary"}/>  
     </View>
   );
 }
