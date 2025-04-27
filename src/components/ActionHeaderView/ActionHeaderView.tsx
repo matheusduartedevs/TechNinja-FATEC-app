e0c57516b0b6f8277ea9dd570346d1142136459a
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-04-27 16:06:02 -0300

    feat: adding routing to header

--- src/components/ActionHeaderView/ActionHeaderView.tsx
+++ src/components/ActionHeaderView/ActionHeaderView.tsx
@@ -1,16 +1,29 @@
 import { ActionHeaderViewProps } from "@/src/components/ActionHeaderView/props";
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import { styles } from "@/src/components/ActionHeaderView/styles";
 import arrowBack from "@/assets/icons/arrow-back.png";
 import TitleView from "@/src/components/TitleView/TitleView";
+import { useRouter } from "expo-router";
 
 export default function ActionHeaderView({
   style,
   title,
 }: ActionHeaderViewProps) {
+  const router = useRouter();
+
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back();
+    } else {
+      router.push("/home");
+    }
+  };
+
   return (
     <View style={[styles.container, style]}>
-      <Image source={arrowBack} style={styles.icon} />
+      <TouchableOpacity onPress={handleBack}>
+        <Image source={arrowBack} style={styles.icon} />
+      </TouchableOpacity>
       <TitleView title={title} color={"primary"} style={styles.title} />
     </View>
   );
