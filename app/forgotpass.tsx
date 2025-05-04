a10d1f43ca221eca3b78baee95b1961ec8bb579d
Julia Ayumi <julia.uno@fatec.sp.gov.br>
2025-05-03 22:18:11 -0300

    feat: implementing forgotpass screen

---
+++ app/forgotpass.tsx
@@ -0,0 +1,92 @@
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import designSystem from "@/src/styles/theme";
+import ButtonView from "@/src/components/ButtonView/ButtonView";
+import InputView from "@/src/components/InputView/InputView";
+import iconEmail from "@/assets/icons/icon.png";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import TextView from "@/src/components/TextView/TextView";
+import { useAuth } from "@/src/hooks/AuthContext";
+import ActionHeaderView from "@/src/components/ActionHeaderView/ActionHeaderView";
+
+export default function ForgotpassScreen() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+
+  const {} = useAuth();
+
+  const handleForgotpass = async () => {
+    router.push("/login");
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={styles.container}>
+            <ActionHeaderView style={styles.header} title="" />
+
+            <TextView
+              text="Identifique-se para receber um e-mail com as instruções
+              para redefinir sua senha"
+              color="primary"
+              style={styles.text}
+            />
+
+            <InputView
+              logo={iconEmail}
+              placeholder="E-mail"
+              onChangeText={setEmail}
+              value={email}
+            />
+
+            <ButtonView
+              text={"Enviar email"}
+              color={"primary"}
+              onPress={handleForgotpass}
+              style={styles.button}
+            />
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: designSystem.colors.action.primaryBackground,
+    justifyContent: "center",
+    gap: "3%",
+    paddingBottom: "20%",
+  },
+
+  button: {
+    alignSelf: "center",
+  },
+
+  header: {},
+
+  text: {
+    fontFamily: designSystem.fonts.primaryRegular,
+    color: designSystem.colors.text.primary,
+    fontSize: designSystem.fonts.textSize,
+    textAlign: "center",
+    marginHorizontal: "10%",
+  },
+});
