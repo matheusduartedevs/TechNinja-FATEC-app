04094aeaefd712b2b5137775fb864b2e65f401b0
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-05-11 16:37:01 -0300

    feat: adjusting quiz components with accessibility modes

--- src/components/AnswerView/AnswerView.tsx
+++ src/components/AnswerView/AnswerView.tsx
@@ -3,6 +3,7 @@ import { TouchableOpacity } from "react-native";
 import { styles } from "@/src/components/AnswerView/styles";
 import TextView from "@/src/components/TextView/TextView";
 import designSystem from "@/src/styles/theme";
+import { useAccessibility } from "@/src/hooks/AccessibilityContext";
 
 export default function AnswerView({
   answer,
@@ -12,6 +13,8 @@ export default function AnswerView({
   isWrong,
   onPress,
 }: AnswerViewProps) {
+  const { settings } = useAccessibility();
+
   return (
     <TouchableOpacity
       onPress={onPress}
@@ -27,7 +30,16 @@ export default function AnswerView({
         isWrong && styles.wrong,
       ]}
     >
-      <TextView color={"secondary"} text={answer} style={styles.answer} />
+      <TextView
+        color={"secondary"}
+        text={answer}
+        style={[
+          styles.answer,
+          settings.lowVisionMode && {
+            fontSize: designSystem.fonts.lowVisionTextSize,
+          },
+        ]}
+      />
     </TouchableOpacity>
   );
 }
